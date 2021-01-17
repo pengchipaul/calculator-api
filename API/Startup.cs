edfd33ad8f2139550c4a255fb1ff97aaf97b8176
Paul <pengchipaul@gmail.com>
2021-01-18 00:44:25 +1100

    enable CORS

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,11 @@ namespace API
                 .AddFluentValidation( cfg => {
                     cfg.RegisterValidatorsFromAssemblyContaining<AddValidator>();
                 });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,7 +50,7 @@ namespace API
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
